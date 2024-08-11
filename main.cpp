6a423f154f17495e82ccbeb6e36d4fc562e81825
clayicarus <1072464850@qq.com>
2024-08-11 18:50:02 +0800

    feat: use config.json

--- main.cpp
+++ main.cpp
@@ -3,22 +3,28 @@
 //
 
 #include "SocksServer.h"
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
+#include <memory>
 #include <muduo/net/EventLoop.h>
+#include <stdexcept>
 #include <string>
 #include "EncodeServer.h"
 #include "UdpAssociate.h"
 #include "muduo/base/Logging.h"
 #include "muduo/cdns/Resolver.h"
 #include "muduo/net/InetAddress.h"
+#include <nlohmann/json.hpp>
+#include <string_view>
 
 using namespace muduo::net;
 using namespace muduo;
+using json = nlohmann::json;
 
-inline int parseStringLevel(std::string level)
+inline Logger::LogLevel parseStringLevel(std::string_view level)
 {
-    for (auto &i : level) {
+    for (auto i : level) {
         i |= (2 << 4);
     }
     if (level == "trace") return Logger::TRACE;
@@ -27,50 +33,115 @@ inline int parseStringLevel(std::string level)
     if (level == "warn") return Logger::WARN;
     if (level == "error") return Logger::ERROR;
     if (level == "fatal") return Logger::FATAL;
-    return -1;
+    throw std::runtime_error("invalid logLevel");
 }
+
+void updateJsonConfig(json &j1, const json &j2)
+{
+    for (auto &i : j1.items()) {
+        auto key = i.key();
+        if (!j2.count(key)) {
+            continue;
+        }
+        if (!j1[key].is_object()) {
+            j1[key] = j2[key];
+        } else {
+            updateJsonConfig(j1[key], j2[key]);
+        }
+    }
+}
+
+constexpr char defautConfig[] = 
+R"({
+    "logLevel": "warn",
+    "encodeServer": {
+        "enable": true,
+        "port": 6011
+    },
+    "socksServer": {
+        "enable": true,
+        "port": 2333,
+        "authentication": {
+            "usePassword": true,
+            "password": null
+        },
+        "udpAssociation": {
+            "enable": true,
+            "hostname": "localhost",
+            "port": 11451
+        },
+        "highWaterMark": 1024,
+        "maxConnNum": 163,
+        "ignoreLocal": true
+    }
+})";
+constexpr char configPath[] = "config.json";
 
 int main(int argc, char *argv[])
 {
-    int level { Logger::WARN };
-    uint16_t socks_port { 2333 }, association_port { 11451 }, encoder_port { 6011 };
-    std::string association_address { "0.0.0.0" };
-    switch (argc) {
-        default:
-        printf("Usage: socks5 [log level] [assosiation_addr] [association_port] [socks_port] [encoder_port]\n");
-        exit(-1);
-        case 6:
-        encoder_port = atoi(argv[5]);
-        case 5:
-        socks_port = atoi(argv[4]);
-        case 4:
-        association_port = atoi(argv[3]);
-        case 3:
-        association_address = argv[2];
-        case 2:
-        if (argv[1] == std::string("-h") || argv[1] == std::string("--help")) {
-            printf("Usage: socks5 [log level] [assosiation_addr] [association_port] [socks_port] [encoder_port]\n");
-            exit(-1);
+    json config = json::parse(defautConfig);
+    {
+        char buf[1024];
+        auto fp = fopen(configPath, "r");
+        if (fp) {
+            std::string data;
+            std::size_t sz;
+            do {
+                sz = fread(buf, 1, sizeof(buf), fp);
+                data.append(buf, sz);
+            } while (sz > 0);
+            fclose(fp);
+            fp = nullptr;
+            json tempConfig = json::parse(data);
+            if (!tempConfig.empty()) {
+                updateJsonConfig(config, tempConfig);
+            }
+        } else {
+            auto fp = fopen(configPath, "w");
+            if (fp) {
+                fwrite(defautConfig, 1, strlen(defautConfig), fp);
+                fclose(fp);
+                fp = nullptr;
+            }
         }
-        level = parseStringLevel(argv[1]);
-        if (level == -1) {
-            printf("Invalid log level\n");
-            exit(-1);
-        }
-        case 1:
-        break;
     }
-    Logger::setLogLevel(static_cast<Logger::LogLevel>(level));
-    InetAddress socks_address(socks_port), encoder_address(encoder_port), udp_local_address(association_port);
+    
+    Logger::setLogLevel(parseStringLevel(config["logLevel"].get<std::string>()));
     EventLoop loop;
-    SocksServer socksServer(&loop, socks_address);
-    EncodeServer encodeServer(&loop, encoder_address);
-    UdpAssociation udpAssociation(&loop, udp_local_address);
-    cdns::Resolver resolver(&loop);
-    resolver.resolve(association_address, [association_port, &socksServer](const InetAddress &association_addr) {
-        socksServer.setAssociationAddr({ association_addr.toIp(), association_port });
-    });
-    socksServer.start();
-    encodeServer.start();
+    
+    std::unique_ptr<EncodeServer> encodeServer = nullptr;
+    json encodeConfig = config["encodeServer"];
+    if (encodeConfig["enable"]) {
+        InetAddress encodeAddr(encodeConfig["port"].get<uint16_t>());
+        encodeServer = std::make_unique<EncodeServer>(&loop, encodeAddr);
+    }
+    
+    std::unique_ptr<SocksServer> socksServer = nullptr;
+    std::unique_ptr<UdpAssociation> udpAssociation = nullptr;
+    std::unique_ptr<cdns::Resolver> resolver = nullptr;
+    json socksConfig = config["socksServer"];
+    if (socksConfig["enable"]) {
+        InetAddress socksAddr(socksConfig["port"].get<uint16_t>());
+        socksServer = std::make_unique<SocksServer>(&loop, 
+                                                    socksAddr, 
+                                                    socksConfig["ignoreLocal"],
+                                                    socksConfig["maxConnNum"],
+                                                    socksConfig["highWaterMark"]);
+        json assoConfig = socksConfig["udpAssociation"];
+        if (assoConfig["enable"]) {
+            InetAddress assoAddr(assoConfig["port"].get<uint16_t>());
+            udpAssociation = std::make_unique<UdpAssociation>(&loop, assoAddr);
+            resolver = std::make_unique<cdns::Resolver>(&loop);
+            resolver->resolve(assoConfig["hostname"].get<std::string>(), [assoAddr, &socksServer](const InetAddress &association_addr) {
+                socksServer->setAssociationAddr({ association_addr.toIp(), assoAddr.port() });
+            });
+        }
+    }
+    if (encodeServer)
+        encodeServer->start();
+    if (socksServer)
+        socksServer->start();
+    
+    LOG_WARN << "loop-" << &loop << " start";
     loop.loop();
 }
